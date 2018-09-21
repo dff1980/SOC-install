a88456592b3f03c796e65904c6397134e0a75c9b
dff1980 <pp.zhukov@gmail.com>
2018-09-21 19:30:29 +0300

    Update repo-cp.sh

--- repo-cp.sh
+++ repo-cp.sh
@@ -1,4 +1,4 @@
 mkdir -p /srv/www/suse-12.3/x86_64/repos/Cloud
-mount ./SUSE-OPENSTACK-CLOUD-8-x86_64-GM-DVD1.iso /mnt
+mount -o loop ./SUSE-OPENSTACK-CLOUD-8-x86_64-GM-DVD1.iso /mnt
 rsync -avP /mnt/ /srv/www/suse-12.3/x86_64/repos/Cloud/
 umount /mnt
